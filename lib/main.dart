d72c958f3d87034f90a07ddd6584443d0dae6b04
yildiz96kubra <83513264+yildiz96kubra@users.noreply.github.com>
2022-07-31 20:30:40 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_infinite_list/app.dart';
+import 'package:flutter_infinite_list/simple_bloc_observer.dart';
+
+void main() {
+  BlocOverrides.runZoned(
+    () => runApp(
+      const App(),
+    ),
+    blocObserver: SimpleBlocObserver(),
+  );
+}
---
+++ lib/simple_bloc_observer.dart
@@ -0,0 +1,21 @@
+// ignore_for_file: avoid_print
+
+// ignore: depend_on_referenced_packages
+import 'package:bloc/bloc.dart';
+
+class SimpleBlocObserver extends BlocObserver {
+  @override
+  void onTransition(
+    Bloc<dynamic, dynamic> bloc,
+    Transition<dynamic, dynamic> transition,
+  ) {
+    super.onTransition(bloc, transition);
+    print(transition);
+  }
+
+  @override
+  void onError(BlocBase<dynamic> bloc, Object error, StackTrace stackTrace) {
+    print(error);
+    super.onError(bloc, error, stackTrace);
+  }
+}
